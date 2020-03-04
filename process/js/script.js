706d11c2f24c1725ecf84b5986eaebebdee7288c
kathirr007 <kkumarasamy@edit-place.com>
2020-03-04 15:42:49 +0530

    Updated fetch method of getting data

--- process/js/script.js
+++ process/js/script.js
@@ -8,7 +8,22 @@
                 console.log(data)
             })
     }) */
-var artists = fetch('js/data_artists.json')
+/* var artists = fetch('js/data_artists.json')
+    .then(function (res) {
+        return res.json()
+    });
+document.querySelector('.load-info')
+    .addEventListener('click', function(){
+        artists.then(function(data) {
+                console.log(data)
+            })
+    }) */
+
+    var getArtists = new Request('js/data_artists.json', {
+        method: 'GET'
+    })
+
+var artists = fetch(getArtists)
     .then(function (res) {
         return res.json()
     });
