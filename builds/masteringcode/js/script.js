0382111509d163d4fcda68164b9d362ee46845e0
kathirr007 <kkumarasamy@edit-place.com>
2020-03-04 16:55:40 +0530

    Render json to DOM | done

--- builds/masteringcode/js/script.js
+++ builds/masteringcode/js/script.js
@@ -1,14 +1,28 @@
-var data;
+var data, DOMNode;
 var request = new XMLHttpRequest();
+DOMNode = document.querySelector('.artists');
 
 request.open('GET', 'js/data.json');
 
 request.onreadystatechange = function () {
-  if (request.status === 200 && request.readyState === 4) {
-    data = JSON.parse(request.responseText);
-    console.log(data);
-    console.log(request);
-  }
+    if (request.status === 200 && request.readyState === 4) {
+        // console.log(typeof(request.responseText))
+        // console.log(typeof(JSON.parse(request.responseText)))
+        data = JSON.parse(request.responseText);
+        console.log(data);
+        console.log(request);
+
+        for (var item in data.artists) {
+            if (data.artists.hasOwnProperty(item)) {
+                var element = data.artists[item];
+                var listItem = document.createElement('div');
+                listItem.className = 'artist';
+                listItem.innerHTML = '<h4>' + element.name + '</h4>' + '<p>' + element.bio + '</p>';
+
+                DOMNode.appendChild(listItem);
+            }
+        }
+    }
 };
 
 request.send();
--- process/js/script.js
+++ process/js/script.js
@@ -1,5 +1,6 @@
-var data;
+var data, DOMNode;
 var request = new XMLHttpRequest();
+DOMNode = document.querySelector('.artists')
 
 request.open('GET', 'js/data.json');
 
@@ -8,9 +9,23 @@ request.onreadystatechange = function() {
     request.status === 200 &&
     request.readyState === 4
   ) {
-    data = JSON.parse(request.responseText);
-    console.log(data);
-    console.log(request);
+        // console.log(typeof(request.responseText))
+        // console.log(typeof(JSON.parse(request.responseText)))
+        data = JSON.parse(request.responseText);
+        console.log(data);
+        console.log(request);
+
+        for (var item in data.artists) {
+            if (data.artists.hasOwnProperty(item)) {
+                var element = data.artists[item]
+                var listItem = document.createElement('div')
+                listItem.className = 'artist'
+                listItem.innerHTML = '<h4>' + element.name + '</h4>' +
+                '<p>' + element.bio + '</p>'
+
+                DOMNode.appendChild(listItem)
+            }
+        }
   }
 }
 
