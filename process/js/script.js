d50e4ebc30f7882aacc1720f319007dde111296a
kkumarasamy <kkumarasamy@edit-place.com>
2020-03-06 17:29:12 +0530

    Update closures in javascript

--- process/js/script.js
+++ process/js/script.js
@@ -20,13 +20,37 @@ function handleItem(id, text) {
   newElement.innerHTML =  text;
   
   targetElement= document.querySelector(id).parentNode;
-  targetElement.insertBefore(newElement, targetElement.childNodes[2]);
+  targetElement.insertBefore(newElement, targetElement.childNodes[2]); //insertBefor code
+  // targetElement.insertBefore(newElement, targetElement.childNodes[3].nextSibling); // insertAfter code
 }
 
+function removeAlert(){
+  var currentAlert = document.querySelector('#currentAlert');
+  if (currentAlert !== null) {
+    currentAlert.parentNode.removeChild(currentAlert);
+  }
+}
+
+function initItem(id, text) {
+  return function() {
+    handleItem(id, text)
+  }
+}
+function removeCurrentAlert () {
+  return function() {
+    removeAlert()
+  }
+  // document.querySelector(id).parentNode.removeChild(currentAlert)
+}
 
 function setupitem() {
   var DOMElement;
 
+  for(var i=0; i<itemText.length; i++) {
+    DOMElement = document.querySelector(itemText[i].id)
+    DOMElement.addEventListener('focus', initItem(itemText[i].id, itemText[i].text))
+    DOMElement.addEventListener('blur', removeCurrentAlert())
+  }
 } // setupitem
 
 setupitem();
